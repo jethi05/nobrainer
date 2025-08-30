33b5d1c9d74a14d6b8576e79f759ad0df27009d2
jethi05 <jethi.gei@gmail.com>
2025-08-30 18:33:32 +0200

    datein lesbar und schreibbar gelernt

--- testing/file_making/src/main.rs
+++ testing/file_making/src/main.rs
@@ -1,3 +1,27 @@
+use std::fs::File;
+use std::io::Read;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    // Read a file in the local file System
+    let mut data_file = File::open("data.txt").unwrap();
+
+    // Create an empty mutable String
+    let mut file_content = String::new();
+
+    // Cop contets of file to a mutable String
+    data_file.read_to_string(&mut file_content).unwrap();
+
+    println!("File content: {:?}", file_content);
+
+    // try to copy file in other file
+    let mut data_file_cp = File::create("data.cp").expect("creation failed");
+
+    // write contetns to the file
+    data_file_cp.write(file_content.as_bytes()).expect("write failed");
+
+    println!("Created and copied");
 }
+//https://www.programiz.com/rust/file-handling
+
+// Daten aus einer Datei erfolgreich in eine andere Datei gepackt...
